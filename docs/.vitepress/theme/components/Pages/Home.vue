1b39d2058d799adb4afa6166389519dc228feaaf
fatpandac <i@fatpandac.com>
2025-11-24 16:24:25 +0800

    feat: add view all post link

--- docs/.vitepress/theme/components/Pages/Home.vue
+++ docs/.vitepress/theme/components/Pages/Home.vue
@@ -21,7 +21,7 @@ const frontmatter = computed(() => page.value?.frontmatter || {});
     <div
       class="w-full flex items-center justify-center flex-col gap-6 text-center md:(flex-row px-20 gap-12 justify-start) lg:(px-40)">
       <img class="w-40 h-40" :src="frontmatter.hero.avatar" />
-      <div class="h-40 flex flex-col gap-4 items-center md:items-start">
+      <div class="md:h-40 flex flex-col gap-4 items-center md:items-start">
         <h1 class="text-4xl font-bold">{{ frontmatter.hero.title }}</h1>
         <p class="block align-start text-balance md:(text-start)">
           {{ frontmatter.hero.subtitle }}
@@ -29,14 +29,21 @@ const frontmatter = computed(() => page.value?.frontmatter || {});
       </div>
     </div>
     <Running />
-    <div class="w-full text-2xl font-bold text-start px-20 lg:px-40">
-      Recent Posts
+    <div class="flex items-baseline justify-between w-full md:px-20 lg:px-40">
+      <div class="text-2xl font-bold text-start">
+        Recent Posts
+      </div>
+      <div class="flex items-center mb-2 cursor-pointer text-blue-600" @click="router.go('/posts')">
+        View All Posts
+        <div class="i-solar:alt-arrow-right-line-duotone inline-block ml-1" />
+      </div>
     </div>
     <section class="w-full">
       <ul class="w-full p-0! m-0! list-none!">
         <li v-for="post in posts.slice(0, 10)" :key="post.url" @click="router.go(post.url)"
           class="p-2 cursor-pointer md:(flex items-center justify-center mb-4 mx-20) lg:(mx-40) hover:(text-blue-600 bg-blue-50 rounded dark:(bg-gray-800 text-blue-400))">
-          <v-p-badge :text="post.categories.at(0)" type="info" class="mr-2 bg-blue-100 dark:bg-gray-700 translate-y-0" />
+          <v-p-badge :text="post.categories.at(0)" type="info"
+            class="mr-2 bg-blue-100 dark:bg-gray-700 translate-y-0" />
           <span class="inline-block align-middle flex-1 line-clamp-1">{{ post.title }}</span>
           <span class="block lg:inline text-gray-600 text-sm mt-1 text-right md:(mt-0)">{{
             dayjs(post.date).locale(currentLang).format('MMMM D, YYYY') }}</span>
